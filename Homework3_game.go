5b5e2699d4dfba9d08fd8709370cdfb03532fde1
Daria Iaroshchuk <daria.iaroshchuk@gmail.com>
2024-05-25 13:53:05 +0300

    +func getUp, +func checkPockets

--- Homework3_game.go
+++ Homework3_game.go
@@ -3,16 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Аліса прокинулась у зачарованому лісі.")
 
-	isAround := whatisAround{
-		enchantedHouse: "будиночок лісника",
-		path:           "стежка до озера",
-		lake:           "озеро",
-		tree:           "зачароване дерево",
-	}
-
-	isAround.printArea()
+	getUp()
 
 	fmt.Printf("\n Оберіть шлях. Варіанти: 1 - будиночок лісника; 2 - озеро; 3 - зачароване дерево.\n")
 	var wheretogo int
@@ -36,34 +28,10 @@ func main() {
 		}
 	}
 
-	leftPocket := whatisinPocket{
-		coockie: coockie{cookietobeSmall: "печиво, яке робить тебе меньшим", coockietobeBig: "печиво, яке робить тебе більшим", coockietobeGreen: "печиво, яке робить тебе зеленим"},
-	}
-	rightPocket := whatisinPocket{
-		matches: "сірники",
-	}
-
 	if wheretogo == 1 {
 		fmt.Println("\n\nАліса підійшла до будиночку. Двері масивні та старі і Алісі не вистачило сили їх відкрити. Була лише невелика щілина, через яку було видно охайну кухню з безліччу баночок з травами. Видно було, що в будинку давно ніхто не жив. Через щілину Алісі ніяк не пролізти. І тут вона відчула щось у кармані. Можливо вона знайде те, що допоможе їй зайти в будинок.")
 
-		fmt.Printf("\n ---> Щоб перевірити лівий карман введіть 'left', щоб перевірити лівий карман введіть 'right'. Введіть 'stop' для завершення")
-		var checkPocket string
-		fmt.Scan(&checkPocket)
-
-		for checkPocket != "stop" {
-			if checkPocket == "left" {
-				fmt.Println("\nАліса перевірила лівий карман")
-				leftPocket.printPocket()
-			} else {
-				if checkPocket == "right" {
-					fmt.Println("\n---> Аліса перевірила правий карман")
-					rightPocket.printPocket()
-				}
-
-			}
-			fmt.Printf("\n\n перевірте інший карман або завершіть перевірку ввівши 'stop'   ")
-			fmt.Scan(&checkPocket)
-		}
+		checkPokets()
 
 		fmt.Printf("\n ---> використайте один з предметів знайдених у карманах щоб зайти до будинку. \nвведіть 1 - щоб використати печиво, яке робить вас меньшим; \n введіть 2 - щоб використати печиво, яке робить вас більшим; \n введіть 3 - щоб використати печиво, яке робить вас зеленим; \n введіть 4 - щоб використати сірники.")
 		var useItem int
@@ -91,24 +59,7 @@ func main() {
 	if wheretogo == 2 {
 		fmt.Println("\n\nАліса підійшла до озера. Там сиділи три жаби. Вона захотіла спитати їх як вийти з лісу, але вони не розмоаляли ні з ким у лісі хто не був зеленого кольору як вони. 'Що ж робити' подумала Аліса. Аж раптом вона відчула щось у кармані. Можливо вона знайде те, що допоможе їй поговорити з жабами.")
 
-		fmt.Printf("\n ---> Щоб перевірити лівий карман введіть 'left', щоб перевірити лівий карман введіть 'right'. Введіть 'stop' для завершення")
-		var checkPocket string
-		fmt.Scan(&checkPocket)
-
-		for checkPocket != "stop" {
-			if checkPocket == "left" {
-				fmt.Println("\nАліса перевірила лівий карман")
-				leftPocket.printPocket()
-			} else {
-				if checkPocket == "right" {
-					fmt.Println("\n---> Аліса перевірила правий карман")
-					rightPocket.printPocket()
-				}
-
-			}
-			fmt.Printf("\n\n перевірте інший карман або завершіть перевірку ввівши 'stop'   ")
-			fmt.Scan(&checkPocket)
-		}
+		checkPokets()
 
 		fmt.Printf("\n ---> використайте один з предметів знайдених у карманах щоб поговорити з жабами. \nвведіть 1 - щоб використати печиво, яке робить вас меньшим; \n введіть 2 - щоб використати печиво, яке робить вас більшим; \n введіть 3 - щоб використати печиво, яке робить вас зеленим; \n введіть 4 - щоб використати сірники.")
 		var useItem int
@@ -137,24 +88,7 @@ func main() {
 	if wheretogo == 3 {
 		fmt.Println("\n\nАліса підійшла до зачарованого дерева та почула як хтось плаче. То була маленька фея ельф, яка розповіла, що подув сильний вітер та потушив ліхтар-свічку і тепер її сестра заблукає і не зможе повернутися додому адже орієнтується на світло від ліхтаря. І Аліса вона відчула щось у кармані. Можливо вона знайде те, що допоможе вирішити проблему феї.")
 
-		fmt.Printf("\n ---> Щоб перевірити лівий карман введіть 'left', щоб перевірити лівий карман введіть 'right'. Введіть 'stop' для завершення")
-		var checkPocket string
-		fmt.Scan(&checkPocket)
-
-		for checkPocket != "stop" {
-			if checkPocket == "left" {
-				fmt.Println("\nАліса перевірила лівий карман")
-				leftPocket.printPocket()
-			} else {
-				if checkPocket == "right" {
-					fmt.Println("\n---> Аліса перевірила правий карман")
-					rightPocket.printPocket()
-				}
-
-			}
-			fmt.Printf("\n\n перевірте інший карман або завершіть перевірку ввівши 'stop'   ")
-			fmt.Scan(&checkPocket)
-		}
+		checkPokets()
 
 		fmt.Printf("\n ---> використайте один з предметів знайдених у карманах щоб запалити свічку. \nвведіть 1 - щоб використати печиво, яке робить вас меньшим; \n введіть 2 - щоб використати печиво, яке робить вас більшим; \n введіть 3 - щоб використати печиво, яке робить вас зеленим; \n введіть 4 - щоб використати сірники.")
 		var useItem int
@@ -205,4 +139,49 @@ type whatisinPocket struct {
 
 func (b whatisinPocket) printPocket() {
 	fmt.Printf("В кармані вона знайшла:%s, %s, %s, %s.", b.cookietobeSmall, b.coockietobeBig, b.coockietobeGreen, b.matches)
+}
+
+func getUp() {
+
+	fmt.Println("Аліса прокинулась у зачарованому лісі.")
+
+	isAround := whatisAround{
+		enchantedHouse: "будиночок лісника",
+		path:           "стежка до озера",
+		lake:           "озеро",
+		tree:           "зачароване дерево",
+	}
+
+	isAround.printArea()
+
+}
+
+func checkPokets() {
+
+	leftPocket := whatisinPocket{
+		coockie: coockie{cookietobeSmall: "печиво, яке робить тебе меньшим", coockietobeBig: "печиво, яке робить тебе більшим", coockietobeGreen: "печиво, яке робить тебе зеленим"},
+	}
+	rightPocket := whatisinPocket{
+		matches: "сірники",
+	}
+
+	fmt.Printf("\n ---> Щоб перевірити лівий карман введіть 'left', щоб перевірити лівий карман введіть 'right'. Введіть 'stop' для завершення")
+	var checkPocket string
+	fmt.Scan(&checkPocket)
+
+	for checkPocket != "stop" {
+		if checkPocket == "left" {
+			fmt.Println("\nАліса перевірила лівий карман")
+			leftPocket.printPocket()
+		} else {
+			if checkPocket == "right" {
+				fmt.Println("\n---> Аліса перевірила правий карман")
+				rightPocket.printPocket()
+			}
+
+		}
+		fmt.Printf("\n\n перевірте інший карман або завершіть перевірку ввівши 'stop'   ")
+		fmt.Scan(&checkPocket)
+	}
+
 }
